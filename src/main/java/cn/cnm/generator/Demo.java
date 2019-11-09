4782a42011f901a2bccc567b7dd50ff80575ad5b
ziyaliuye <171242039@qq.com>
2019-11-09 11:28:15 +0800

    补充代码...

---
+++ src/main/java/cn/cnm/generator/Demo.java
@@ -0,0 +1,15 @@
+package cn.cnm.generator;
+
+/**
+ * @author lele
+ * @version 1.0
+ * @Description
+ * @Email 414955507@qq.com
+ * @date 2019/11/9 11:18
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+
+    }
+}
