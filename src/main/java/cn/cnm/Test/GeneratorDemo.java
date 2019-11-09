28ec9d338bb0aee3c49de1d99708d0c1189c6409
ziyaliuye <171242039@qq.com>
2019-11-09 13:30:36 +0800

    完善代码...

--- src/main/java/cn/cnm/Test/GeneratorDemo.java
+++ src/main/java/cn/cnm/Test/GeneratorDemo.java
@@ -1,14 +1,12 @@
 package cn.cnm.Test;
 
-import cn.cnm.mapper.FlowerAnnotationMapper;
-import cn.cnm.mapper.PeopleMapper;
-import cn.cnm.pojo.Flower;
-import cn.cnm.pojo.People;
+import cn.cnm.mapper.AirportMapper;
+import cn.cnm.pojo.Airport;
+import cn.cnm.pojo.AirportExample;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -32,11 +30,25 @@ public class GeneratorDemo {
             SqlSession session = factory.openSession();
 
             // 获取接口的实现类, 由MyBatis自动产生代理对象
-            PeopleMapper peopleMapper = session.getMapper(PeopleMapper.class);
-            // 开始调用实例的方法， MyBatis会自动寻找对应的XML文件中的SQL并执行
-            People people = peopleMapper.selectByPrimaryKey(2);
-            System.out.println(people);
-            // 关闭连接， 不然一直处于连接状态
+            AirportMapper airportMapper = session.getMapper(AirportMapper.class);
+            // 除了生成简单的CRUD的实体外， MyBatis还额外生成了强大的查询对象xxxExample（QBC查询）
+            AirportExample airportExample = new AirportExample();
+
+            // 获取实例的Criteria属性， 准备设置条件
+            /* 注意：一个Criteria属性可以拼接多个条件， 条件之间都是and */
+            AirportExample.Criteria criteria1 = airportExample.createCriteria();
+            // Criteria：设置查询条件
+            criteria1.andIdBetween(1, 2);
+            criteria1.andCitynameLike("%长沙%");
+            /* 一个Example可以多个Criteria属性， 调用Example的or方法后可以将其设置为or条件 */
+            AirportExample.Criteria criteria2 = airportExample.createCriteria();
+            criteria2.andCitynameLike("%北京%");
+            /* 将criteria2设置为or条件 */
+            airportExample.or(criteria2);
+
+            // 将Example实例传入Mapper中， 开始调用selectByExample()查询
+            List<Airport> list = airportMapper.selectByExample(airportExample);
+            list.forEach(System.out::println);
             session.close();
         } catch (IOException e) {
             e.printStackTrace();
