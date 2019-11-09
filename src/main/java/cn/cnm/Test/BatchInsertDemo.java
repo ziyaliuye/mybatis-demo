1f8b54bb131565c1185451d76004629b39901fd1
ziyaliuye <171242039@qq.com>
2019-11-09 15:37:09 +0800

    完善代码...

---
+++ src/main/java/cn/cnm/Test/BatchInsertDemo.java
@@ -0,0 +1,45 @@
+package cn.cnm.Test;
+
+import cn.cnm.mapper.PeopleMapper;
+import cn.cnm.pojo.People;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+/**
+ * @author lele
+ * @version 1.0
+ * @Description
+ * @Email 414955507@qq.com
+ * @date 2019/11/7 10:10
+ */
+public class BatchInsertDemo {
+    public static void main(String[] args) {
+        InputStream inputStream;
+        try {
+            // 字符流基类， 读取 MyBatis 主配置文件
+            inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+            // 使用构建者模式（名称带 build）简化对象实例化过程
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+            /* 获取sqlSession时需要指定为批量操作， MyBatis会为这些操作做一系列的优化 */
+            SqlSession session = factory.openSession(ExecutorType.BATCH);
+
+            PeopleMapper peopleMapper = session.getMapper(PeopleMapper.class);
+
+            // 插入10000条记录
+            for (int i = 100; i < 10100; i++) {
+                peopleMapper.insert(new People(i, UUID.randomUUID().toString().substring(0, 6), 18, i + 1));
+            }
+
+            session.commit();
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/cn/cnm/pojo/People.java
+++ src/main/java/cn/cnm/pojo/People.java
@@ -1,5 +1,12 @@
 package cn.cnm.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@AllArgsConstructor // 全参构造
+@NoArgsConstructor // 无参构造
+@Accessors(chain = true) // 链式风格访问
 public class People {
     private Integer id;
 
